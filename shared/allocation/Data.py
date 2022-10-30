eb729a3f75bf16a0ad9272dcf3a702d3ccaa019b
Daniel Platkevicius <daniel.platkevicius@unibw.de>
2022-10-30 21:20:15 +0100

    Feature: Types for mi and riscv

--- shared/allocation/Data.py
+++ shared/allocation/Data.py
@@ -1,5 +1,13 @@
 class Data:
-    def __init__(self, name, offset, location):
-        self.name = name
+    def __init__(self, data, offset, location):
+        self.data = data
         self.offset = offset
         self.location = location
+
+        @property
+        def offset():
+            return self.offset
+
+        @offset.setter
+        def offset(value):
+            self.type_def = value
---
+++ shared/variables/Variable.py
@@ -0,0 +1,4 @@
+class Variable:
+    def __init__(self, name, type_def):
+        self.name = name
+        self.type_def = type_def
